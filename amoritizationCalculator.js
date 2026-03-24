1756cb90550915d4cde35e1f32ed778e0fa72f41
Anna Moore <annathekarategirl@gmail.com>
2026-03-24 13:44:55 -0400

    f

--- amoritizationCalculator.js
+++ amoritizationCalculator.js
@@ -1,9 +1,14 @@
+//main issue is unable to access values even with .value
+
 const button1=document.getElementById("Button1")
 const div=document.getElementById("amor")
 button1.addEventListener("click",calc)
 let PrincipalLoanAmount
 let initialLoan
 let term
+let monthlyPayment
+let totalInterestPaid
+let totalLoanCost
 function calc() {
     button1.disabled=true
     div.innerHTML+="<input id='loan'>Loan</input>"
@@ -22,7 +27,16 @@ function calc() {
     const InterestRate=.0575
     const monthlyInterestRate = InterestRate/12
     const totalMonths=term*12
-    let monthlyPayment = ((monthlyInterestRate * principalLoanAmount) / (1 - Math.pow(1 + monthlyInterestRate, -totalMonths))).toFixed(2);
-    let totalInterestPaid=(monthlyPayment*totalMonths)-principalLoanAmount
-    let totalLoanCost=principalLoanAmount+totalInterestPaid
+    monthlyPayment = ((monthlyInterestRate * PrincipalLoanAmount) / (1 - Math.pow(1 + monthlyInterestRate, -totalMonths))).toFixed(2);
+    totalInterestPaid=(monthlyPayment*totalMonths)-PrincipalLoanAmount
+    totalLoanCost=PrincipalLoanAmount+totalInterestPaid
+   console.log(totalMonths)
+    div.innerHTML+="<button id='button3'>Display</button>"
+    document.getElementById("button3").addEventListener("click",display) 
+    function display(){
+ console.log(totalInterestPaid)
+    console.log(term)
+    console.log(monthlyInterestRate)
+    }
+
 }
