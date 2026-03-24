1afe46c8e0b577df1151a161c017a389af804b4f
Anna Moore <annathekarategirl@gmail.com>
2026-03-24 13:52:39 -0400

    v

--- amoritizationCalculator.js
+++ amoritizationCalculator.js
@@ -16,27 +16,34 @@ function calc() {
     div.innerHTML+="<input id='term'>Loan term years</input>"
     div.innerHTML+="<button id='button2'>Submit</button>"
     const button2=document.getElementById("button2") 
-    button2.addEventListener("click",calcmore)
-    function calcmore(){
+    button2.addEventListener("click",calcmore)}
+    
+function calcmore(){
     initialLoan=document.getElementById("loan").value
-    term=Number(document.getElementById("term").value)
+    term=document.getElementById("term").value
     //initialLoan.onchange=function(){initialLoan=initialLoan.value}
     console.log(PrincipalLoanAmount)
-    PrincipalLoanAmount=Number(initialLoan)-(Number(initialLoan)*(Number(document.getElementById("down").value/100)))}
+    PrincipalLoanAmount=Number(initialLoan)-(Number(initialLoan)*(Number(document.getElementById("down").value/100)))
     console.log(initialLoan)
     const InterestRate=.0575
     const monthlyInterestRate = InterestRate/12
-    const totalMonths=term*12
+    const totalMonths=Number(term)*12
     monthlyPayment = ((monthlyInterestRate * PrincipalLoanAmount) / (1 - Math.pow(1 + monthlyInterestRate, -totalMonths))).toFixed(2);
     totalInterestPaid=(monthlyPayment*totalMonths)-PrincipalLoanAmount
     totalLoanCost=PrincipalLoanAmount+totalInterestPaid
-   console.log(totalMonths)
-    div.innerHTML+="<button id='button3'>Display</button>"
-    document.getElementById("button3").addEventListener("click",display) 
-    function display(){
- console.log(totalInterestPaid)
-    console.log(term)
-    console.log(monthlyInterestRate)
-    }
+   console.log(term)
 
+//     div.innerHTML+="<button id='button3'>Display</button>"
+//     document.getElementById("button3").addEventListener("click",display) 
+//     function display(){
+//  console.log(totalInterestPaid)
+//     console.log(term)
+//     console.log(monthlyInterestRate)
+//     }
+        div.innerHTML+="<p>Term:"+term+"</p>"
+        div.innerHTML+="<p>Annual interest rate:"+InterestRate*100+"%</p>"
+         div.innerHTML+="<p>Principal loan amount:"+PrincipalLoanAmount+"</p>"
+          div.innerHTML+="<p>Total interest paid:"+totalInterestPaid+"</p>"
+           div.innerHTML+="<p>Total Loan cost:"+totalLoanCost+"</p>"
+            div.innerHTML+="<p>Monthly payment:"+monthlyPayment+"</p>"
 }
