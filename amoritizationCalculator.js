cb47eebaf47229af1581aaac601033f865248b2e
Anna Moore <annathekarategirl@gmail.com>
2026-03-24 13:34:43 -0400

    d

--- amoritizationCalculator.js
+++ amoritizationCalculator.js
@@ -1,8 +1,28 @@
 const button1=document.getElementById("Button1")
-
+const div=document.getElementById("amor")
 button1.addEventListener("click",calc)
-
+let PrincipalLoanAmount
+let initialLoan
+let term
 function calc() {
-    console.log("Works")
-
+    button1.disabled=true
+    div.innerHTML+="<input id='loan'>Loan</input>"
+    div.innerHTML+="<input id='down'>Down payment %</input>"
+    div.innerHTML+="<input id='term'>Loan term years</input>"
+    div.innerHTML+="<button id='button2'>Submit</button>"
+    const button2=document.getElementById("button2") 
+    button2.addEventListener("click",calcmore)
+    function calcmore(){
+    initialLoan=document.getElementById("loan").value
+    term=Number(document.getElementById("term").value)
+    //initialLoan.onchange=function(){initialLoan=initialLoan.value}
+    console.log(PrincipalLoanAmount)
+    PrincipalLoanAmount=Number(initialLoan)-(Number(initialLoan)*(Number(document.getElementById("down").value/100)))}
+    console.log(initialLoan)
+    const InterestRate=.0575
+    const monthlyInterestRate = InterestRate/12
+    const totalMonths=term*12
+    let monthlyPayment = ((monthlyInterestRate * principalLoanAmount) / (1 - Math.pow(1 + monthlyInterestRate, -totalMonths))).toFixed(2);
+    let totalInterestPaid=(monthlyPayment*totalMonths)-principalLoanAmount
+    let totalLoanCost=principalLoanAmount+totalInterestPaid
 }
